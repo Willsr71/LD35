44d7d49e866e66122483e8e027e62665dc621fd3
Willsr71 <willsr71@gmail.com>
2016-04-18 01:00:03 -0400

    fix number of scores showing

--- php/getscores.php
+++ php/getscores.php
@@ -1,6 +1,6 @@
 <?php
 $database = mysqli_connect("localhost", "ld35", "", "ld35");
-$result = mysqli_query($database, "SELECT `username`, `score` FROM `galaxyshifter_scores` ORDER BY `galaxyshifter_scores`.`score` DESC LIMIT 20;");
+$result = mysqli_query($database, "SELECT `username`, `score` FROM `galaxyshifter_scores` ORDER BY `galaxyshifter_scores`.`score` DESC LIMIT 30;");
 
 $scores = array();
 
